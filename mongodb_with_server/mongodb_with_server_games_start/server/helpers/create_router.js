07be0f46dff614b2086871981c980582bde3d505
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-27 11:09:27 +0000

    router.get works

--- mongodb_with_server/mongodb_with_server_games_start/server/helpers/create_router.js
+++ mongodb_with_server/mongodb_with_server_games_start/server/helpers/create_router.js
@@ -4,6 +4,13 @@ const createRouter = function (collection) {
 
   const router = express.Router();
 
+  router.get('/', (req, res) => {
+    collection
+      .find()      // find returns a mongodb cursor, 
+      .toArray()   // this needs concverted to an array in order to be converted to json, this takes indeterminate time so is a promise.
+      .then(docs => res.json(docs));
+  });
+
   return router;
 
 };
--- mongodb_with_server/mongodb_with_server_games_start/server/server.js
+++ mongodb_with_server/mongodb_with_server_games_start/server/server.js
@@ -11,7 +11,9 @@ MongoClient.connect('mongodb://localhost:27017')      // mongodb not http becaus
     const db = client.db('games_hub');                //  client is banana, it could be connectionToMongoDBLand
     const gamesCollection = db.collection('games');
     const gamesRouter = createRouter(gamesCollection);
+    app.use('/api/games', gamesRouter);
   })
+  .catch(err => console.error(err));      // if anything goes wrong with theis promise chain, catch it (handle that error using this - a catch produces and error, send this error to the console. this stops the error from crashing the whole api)
 
 app.listen(3000, function () {
   console.log(`Listening on port ${ this.address().port }`);
