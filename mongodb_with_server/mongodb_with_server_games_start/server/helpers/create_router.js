cd5f6d4c9a2d454dab228579e93bbddb57cd9938
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-27 11:23:15 +0000

    Wrote get id

--- mongodb_with_server/mongodb_with_server_games_start/server/helpers/create_router.js
+++ mongodb_with_server/mongodb_with_server_games_start/server/helpers/create_router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ObjectId = require('mongodb').ObjectId;   //   imports objectid from mongodb
 
 const createRouter = function (collection) {
 
@@ -8,8 +9,26 @@ const createRouter = function (collection) {
     collection
       .find()      // find returns a mongodb cursor, 
       .toArray()   // this needs concverted to an array in order to be converted to json, this takes indeterminate time so is a promise.
-      .then(docs => res.json(docs));
+      .then(docs => res.json(docs))   // converts our found array to json
+      .catch(err => {   // in the event of error in promise, catch runs
+        console.error(err);   // send error to console (red)
+        res.status(500);      //  default 500 comes from ??????
+        res.json({ status: 500, error: err });  // creates error object - ?????
+      });
   });
+
+  router.get('/:id', (req, res) => {
+    const idAsAString = req.params.id;                    // mongodb stores ids as object ids which are like classes for each id, not a string or int or bool, but an obj.
+    const idAsAMongoDbObjectId = ObjectId(idAsAString);   // this converts our id as a string into a mongodb object id.
+    collection
+      .findOne({ _id: idAsAMongoDbObjectId })             // findOne is passed a QueryObject eg { minPlayers: 2 }, to search on id use _id, so { _id: idAsAMongoDbObjectId }
+      .then(doc => res.json(doc))                         // gives us the document directly not a cursor, so we can just convert it to json and send it to the client
+      .catch(err => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+    });
 
   return router;
 
